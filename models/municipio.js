5ef8b237873b984a68fd73805b7997e3d05bfcca
Alfonso Cobelas <alfonsoalejandro33@gmail.com>
2023-05-21 15:29:14 +0200

    setup project

--- models/municipio.js
+++ models/municipio.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose')
 const municipioSchema = new mongoose.Schema({
     codPro: { type: Number, required: true },
     codMun: { type: Number, required: true },
-    nombreMunicipio: { type:String, required: true }
+    nombre: { type: String, required: true }
 })
 module.exports = mongoose.model('Municipios',municipioSchema,'municipios')
